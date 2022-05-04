3ced914f0a7274e0a0220792289754fd7d865f30
Ondrej Sika <ondrej@ondrejsika.com>
2022-05-04 10:53:59 +0200

    feat: Create simple Terraform example of CI vars configiration

---
+++ .terraform.lock.hcl
@@ -0,0 +1,23 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/terraform-providers/gitlab" {
+  version = "3.14.0"
+  hashes = [
+    "h1:xR1R5xT0jLXzRqqkHLh8fQ6MjHSwvw1qajFulebHHlY=",
+    "zh:1c3e89cf19118fc07d7b04257251fc9897e722c16e0a0df7b07fcd261f8c12e7",
+    "zh:25be08eb83e94820d72884a02684dd38b8a47544dc5afce1640ba25a94720c6f",
+    "zh:2a0b9388a1b7c84afce36df990b4f23de50fb5672538333f46998d14319f213a",
+    "zh:2bdf18cbd144326fc99cf507196ad21d099f0360c16786fb543d575d6770a139",
+    "zh:487890bc773edb3e42831ce649eb627a8a21da0b8874ae068e204c6fb0e834a3",
+    "zh:4e47b4430f9974050188cb09fb9ac4647eb473b04d50165277b290e5e742b6cf",
+    "zh:63735a191a39b177eb4513cea9f4592971a017d5d3bf7942b39d955e960c819a",
+    "zh:6f89bbc30eabc4ae79af0bda57d7d5c037e68c24130348780673662a4fa2bd10",
+    "zh:7d42633babb29f8d86df0e03b1b8738a16f9cba61d3951aca02e32174a11299f",
+    "zh:865d20d3e868ac8ece7fe4294eaca9c85945400e043b74003ef01606bcfb8092",
+    "zh:a747a4b55f25982896939785acba21ac441c8eaf1b906f1bb1dc7388b4f5350b",
+    "zh:b0b2217b2aa97a1aea44d1f58463699663c4bd8832621c36c37617cada8b1ff5",
+    "zh:d48bfcf224c21756a35e150a1cb2cf699ba7ebffc575bf4dd2297e918e82da12",
+    "zh:fa64065c7f37ce4c9052ba9c78db632e2b55806ca53a969febcaa17d210f9644",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    gitlab = {
+      source = "terraform-providers/gitlab"
+    }
+  }
+}
+
+provider "gitlab" {
+  base_url = "https://gitlab.sikademo.com/api/v4/"
+  token    = "yJVPZVTsZ-p7ZQe1c9P1"
+}
+
+locals {
+  example_vars = {
+    FOO = "Hello"
+    BAR = "World"
+  }
+  example_masked_vars = {
+    SECRET = "thisissecret"
+  }
+  example_ondrejsika_vars = {
+    HELLO = "ahoj"
+  }
+}
+
+data "gitlab_group" "example" {
+  full_path = "example"
+}
+
+resource "gitlab_group_variable" "example" {
+  for_each = local.example_vars
+
+  group             = data.gitlab_group.example.id
+  key               = each.key
+  value             = each.value
+  protected         = false
+  masked            = false
+  environment_scope = "*"
+}
+
+resource "gitlab_group_variable" "example_masked" {
+  for_each = local.example_masked_vars
+
+  group             = data.gitlab_group.example.id
+  key               = each.key
+  value             = each.value
+  protected         = false
+  masked            = true
+  environment_scope = "*"
+}
+
+
+data "gitlab_project" "example_ondrejsika" {
+  id = "example/ondrejsika"
+}
+
+resource "gitlab_project_variable" "example_ondrejsika" {
+  for_each = local.example_ondrejsika_vars
+
+  project           = data.gitlab_project.example_ondrejsika.id
+  key               = each.key
+  value             = each.value
+  protected         = false
+  masked            = false
+  environment_scope = "*"
+}
